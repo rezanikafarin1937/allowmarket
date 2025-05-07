cfcedaed33ab1b99f9e84b651e3ff692a592045a
rezanikafarin1937 <rezanikafarin700@gmail.com>
2025-05-07 01:24:40 -0700

    modified:   ../.gitignore

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,11 @@ import style from "./Header.module.scss";
 const Header = () => {
   return (
     <div className={style.header}>
-      <p>Header</p>
+      <div className={style.header__container}>
+        <div className={style.header__nav}>PROJECTS</div>
+        <div className={style.header__nav}>MIKIYA KOBAYASHI</div>
+        <div className={style.header__nav}>ALL</div>
+      </div>
     </div>
   );
 };
